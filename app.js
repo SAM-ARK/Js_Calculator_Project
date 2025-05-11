eb6049d2c129645ceb6ae69d02e18428400d51db
tebatso sam <tebat@Sam-Pharos.mtn.co.za>
2025-05-11 13:03:53 +0200

    updated UI

--- app.js
+++ app.js
@@ -4,23 +4,29 @@
 const display = document.querySelector('#display');
 
 
-//append funtion to display
+//append function to display
 function appendToDisplay(value) {
     display.value += value;
+    console.log(display.value);
 }
 
 //function to clear display
-
 function clearDisplay() {
     display.value = '';
 }
 
 //function to calculate the result
 function calculate() {
-    try {
+
+    if (display.value !== '') {
+       try {
         display.value = eval(display.value);
     }
     catch (error) {
         display.value = 'Error';
     }
+    } else {
+        display.value = "Please Enter a Value";
+    }
 }
+
